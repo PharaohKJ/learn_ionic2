299420f95d70f2a55d0f907d19f1b0390acd811f
Masayuki Shimizu <masayuki_shimizu@2rhythm.jp>
2017-10-19 23:43:59 +0900

    androidでdebug（うまくいかない

--- src/providers/fb-service/fb-service.ts
+++ src/providers/fb-service/fb-service.ts
@@ -19,8 +19,6 @@ export class FbServiceProvider {
     this.fb.login(['public_profile', 'user_friends', 'email'])
       .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
       .catch(e => console.log('Error logging into Facebook', e));
-
-    this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
   }
 
 
